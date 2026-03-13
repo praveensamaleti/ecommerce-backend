80cd9fbf868966cff6e42564840fd993cc1a2920
samaletipraveen <samaletipraveen@gmail.com>
2026-03-13 21:20:29 +0530

    add more mock data

--- src/main/java/com/ecommerce/backend/entity/Order.java
+++ src/main/java/com/ecommerce/backend/entity/Order.java
@@ -33,6 +33,7 @@ public class Order {
     private OrderStatus status;
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
     private List<OrderItem> items = new ArrayList<>();
 
     @Embedded
